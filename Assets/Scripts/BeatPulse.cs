ad94ed18c2826664e41baa947e8c25f2ed69ba1d
Fabian <lindgrenfabian@gmail.com>
2020-03-02 07:46:28 +0100

    improved anims

--- Assets/Scripts/BeatPulse.cs
+++ Assets/Scripts/BeatPulse.cs
@@ -1,15 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class BeatPulse : MonoBehaviour
 {
 
     public float BPM = 100f;
+    private bool ready = false;
 
     // Update is called once per frame
+    private void Start()
+    {
+        StartCoroutine(Wait1s());
+    }
     void Update()
     {
         var baseValue = Mathf.Cos(((Time.time * Mathf.PI) * (BPM / 60f)) % Mathf.PI);
+        if(ready)
         this.transform.localScale = Vector3.Lerp(new Vector3(1f, 1f, 1), new Vector3(1.04f,1.04f,1), baseValue);
+    }
+    public IEnumerator Wait1s()
+    {
+        yield return new WaitForSeconds(1f);
+        ready = true;
     }
 }
