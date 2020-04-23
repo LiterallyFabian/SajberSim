33af222ed4f34e8cef7ed480062b37f405aee6ce
Fabian <lindgrenfabian@gmail.com>
2020-04-23 16:52:46 +0200

    removed delay in logo beat

--- Assets/Scripts/BeatPulse.cs
+++ Assets/Scripts/BeatPulse.cs
@@ -6,27 +6,16 @@ public class BeatPulse : MonoBehaviour
 {
 
     public float BPM = 100f;
-    private bool ready = false;
     public GameObject ray;
 
     // Update is called once per frame
-    private void Start()
-    {
-        StartCoroutine(Wait1s());
-    }
+
     void Update()
     {
         var baseValue = Mathf.Cos(Time.time * Mathf.PI * (BPM / 60f) % Mathf.PI);
         ray.transform.Rotate(0, 0, 30 * Time.deltaTime);
-        if (!ready) return;
         this.transform.localScale = Vector3.Lerp(new Vector3(1f, 1f, 1), new Vector3(1.04f,1.04f,1), baseValue);
         ray.transform.localScale = Vector3.Lerp(new Vector3(1f, 1f, 1), new Vector3(1.08f, 1.08f, 1), baseValue);
-
-
     }
-    public IEnumerator Wait1s()
-    {
-        yield return new WaitForSeconds(1f);
-        ready = true;
-    }
+
 }
