c2d0402fdec2bc5d4cd64be6c342c0ae06dab7e7
Fabian <lindgrenfabian@gmail.com>
2020-05-04 16:59:30 +0200

    added unfinished download module

---
+++ Assets/Scripts/Download.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace SajberSim.Web
+{
+    class Download : MonoBehaviour
+    {
+        public IEnumerator UpdateItem(GameObject item, string path, string type)
+        {
+            Debug.Log($"Trying to download file {path}...");
+            using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(path))
+            {
+                yield return uwr.SendWebRequest();
+                if (uwr.isNetworkError) Debug.LogError($"Could not download {path}\n{uwr.error}");
+                else
+                {
+                    var texture = DownloadHandlerTexture.GetContent(uwr);
+                    if(type == "img")
+                        item.GetComponent<Image>().sprite = UnityEngine.Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+                    else if (type == "sprite")
+                        item.GetComponent<SpriteRenderer>().sprite = UnityEngine.Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+                    Debug.Log($"{path} downloaded successfully.");
+                }
+            }
+        }
+        public void Image(GameObject items, string path)
+        {
+            StartCoroutine(UpdateItem(items, path, "img"));
+        }
+        public static void Sprite(GameObject item, string path)
+        {
+
+        }
+    }
+
+}
