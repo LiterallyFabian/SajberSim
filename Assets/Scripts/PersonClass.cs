13d0f89b616e06951f74845462177a7921c904be
Fabian <lindgrenfabian@gmail.com>
2020-05-04 16:59:58 +0200

    Optimized class

--- Assets/Scripts/PersonClass.cs
+++ Assets/Scripts/PersonClass.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PersonClass : MonoBehaviour
+namespace SajberSim.Chararcter
 {
 
     public class Character
---
+++ Assets/Scripts/Story.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Story
+    {
+        public string name;
+        public string path;
+        public int length;
+        public string firstAction;
+        public string[] alltext;
+
+        public Story(string filename)
+        {
+            name = filename;
+            path = $"{Application.dataPath}/Modding/Dialogues/{filename}.txt";
+            if (!File.Exists(path))
+            {
+                Debug.LogError($"Tried to create story \"{filename}\" which does not exist. (PATH: {path})");
+                return;
+            }
+            alltext = File.ReadAllLines($"{Application.dataPath}/Modding/Dialogues/{filename}.txt");
+            length = alltext.Length;
+
+            foreach(string line in alltext)
+            {
+                if (GetActionFromText(line) != "COMMENT" && GetActionFromText(line) != "COMMENT")
+                {
+                    firstAction = GetActionFromText(line);
+                    break;
+                }   
+            }
+        }
+        public string[] GetLine(int line)
+        {
+            return alltext[line].Split('|');
+        }
+        public string GetActionFromLine(int line)
+        {
+            return GetActionFromText(alltext[line]);
+        }
+        public string GetActionFromText(string line)
+        {
+            if (line.StartsWith("//")) return "COMMENT";
+            if (line == "") return "BLANK";
+            else return line.Split('|')[0];
+        }
+
+    }
+}
