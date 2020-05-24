72f0b26e88295d95bf5eebed7dc582309cbcf803
Fabian <lindgrenfabian@gmail.com>
2020-05-24 14:45:57 +0200

    Create helper script

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SajberSim.Helper
+{
+    class Helper
+    {
+        /// <summary>
+        /// Checks if input is an int or not
+        /// </summary>
+        public static bool IsNum(string input)
+        {
+            if (int.TryParse(input, out int n)) return true;
+            else return false;
+        }
+    }
+}
