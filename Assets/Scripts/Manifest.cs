eb36b594dba18abe5eef654f25fa376fe10ea38b
Fabian <lindgrenfabian@gmail.com>
2020-05-26 23:06:50 +0200

    added manifest template class

---
+++ Assets/Scripts/Manifest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class Manifest
+{
+    public string name;
+    public string author;
+    public string description;
+    public string id;
+    public string language;
+    public string overlaycolor;
+    public string textcolor;
+    public bool nsfw;
+}
