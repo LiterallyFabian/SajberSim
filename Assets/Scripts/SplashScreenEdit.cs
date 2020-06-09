8c08bba0a148e202dbd31044d3d3e64ef2f63e5c
Fabian <lindgrenfabian@gmail.com>
2020-06-09 15:44:38 +0200

    Edited splashscreen to support audio

---
+++ Assets/Scripts/SplashScreenEdit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+// This example shows how you could draw the splash screen at the start of a Scene. This is a good way to integrate the splash screen with your own or add extras such as Audio.
+public class SplashScreenEdit : MonoBehaviour
+{
+    public GameObject music;
+    private static bool played = false;
+    IEnumerator Start()
+    {
+        if (Application.isEditor && !played)
+        {
+            played = true;
+            music.GetComponent<AudioSource>().Play();
+            Time.timeScale = 0;
+            SplashScreen.Begin();
+            while (!SplashScreen.isFinished)
+            {
+                SplashScreen.Draw();
+                yield return null;
+            }
+            Time.timeScale = 1;
+        }
+    }
+}
