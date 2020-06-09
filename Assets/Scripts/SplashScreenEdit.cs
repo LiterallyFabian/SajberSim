927770eb1b41b0d959a901cc594b50c4fd007496
Fabian <lindgrenfabian@gmail.com>
2020-06-09 15:55:00 +0200

    Moved login screen

--- Assets/Scripts/SplashScreenEdit.cs
+++ Assets/Scripts/SplashScreenEdit.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-// This example shows how you could draw the splash screen at the start of a Scene. This is a good way to integrate the splash screen with your own or add extras such as Audio.
 public class SplashScreenEdit : MonoBehaviour
 {
     public GameObject music;
