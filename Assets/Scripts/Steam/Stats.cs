8286b9b78a96c7b7f0dc65d4974b41b4c94cc11b
Fabian <lindgrenfabian@gmail.com>
2020-06-21 20:05:42 +0200

    Created helper for modifying stats

---
+++ Assets/Scripts/Steam/Stats.cs
@@ -0,0 +1,30 @@
+﻿using Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SajberSim.Steam
+{
+    public class Stats : MonoBehaviour
+    {
+        public enum List
+        {
+            novelsstarted,
+            novelsfinished,
+            novelscreated,
+            novelspublished
+        }
+        public static void Add(List stat, int value = 1)
+        {
+            SteamUserStats.AddStat(stat.ToString(), value);
+        }
+        public static int GetInt(List stat)
+        {
+            return SteamUserStats.GetStatInt(stat.ToString());
+        }
+        public static float GetFloat(List stat)
+        {
+            return SteamUserStats.GetStatInt(stat.ToString());
+        }
+    }
+}
