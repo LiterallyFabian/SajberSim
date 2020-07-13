b40a05db4500ca903c98a50b85fcc78f4d1c5aa5
Fabian <lindgrenfabian@gmail.com>
2020-07-13 16:39:33 +0200

    sorted manifest variables

--- Assets/Scripts/Manifest.cs
+++ Assets/Scripts/Manifest.cs
@@ -17,20 +17,26 @@ using static SajberSim.Helper.Helper;
 public class Manifest
 {
 #pragma warning disable CS0649
+    //Metadata
     public string name = "Default name";
-    public string author = "Default author";
-    public string authorid = "-1";
     public string description = "Default description";
-    public string id = "0";
     public string language = "US";
-    public string overlaycolor = "FFFFFF";
-    public string textcolor = "323232";
     public bool nsfw = false;
     public int playtime = 0;
     public int publishdate = 19700101;
     public string[] tags = new string[0];
     public string genre = "other";
     public string rating = "everyone";
+
+    //IDs
+    public string author = "Default author";
+    public string authorid = "-1";
+    public string id = "0";
+    
+    //Designs
+    public string overlaycolor = "FFFFFF";
+    public string textcolor = "323232";
+    
 
     public static Manifest Get(string path)
     {
