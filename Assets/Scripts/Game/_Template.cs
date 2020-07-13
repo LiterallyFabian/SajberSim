d93f4c8a8048438fa756087dd9d2cb48a398ea50
Fabian <lindgrenfabian@gmail.com>
2020-07-13 20:38:08 +0200

    added template for game actions

---
+++ Assets/Scripts/Game/_Template.cs
@@ -0,0 +1,21 @@
+﻿using SajberSim.Chararcter;
+using SajberSim.Helper;
+using SajberSim.Translation;
+using SajberSim.Web;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class _Template : MonoBehaviour
+{
+    public GameManager Game;
+    public void Run(string[] line)
+    {
+
+    }
+    public string Working(string[] line)
+    {
+        return "";
+    }
+}
