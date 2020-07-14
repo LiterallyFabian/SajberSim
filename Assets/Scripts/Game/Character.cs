3f4537dbb5c194556ce7e5645004381648e3c49b
Fabian <lindgrenfabian@gmail.com>
2020-07-14 20:47:31 +0200

    create character script template

---
+++ Assets/Scripts/Game/Character.cs
@@ -0,0 +1,27 @@
+﻿using SajberSim.Chararcter;
+using SajberSim.Helper;
+using SajberSim.Translation;
+using SajberSim.Web;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public GameManager Game;
+    public void Run(string[] line)
+    {
+        string status = Working(line);
+        if (status != "")
+        {
+            UnityEngine.Debug.LogWarning($"Error at line {GameManager.dialoguepos} in script {GameManager.scriptPath}: {status}");
+            Helper.Alert(string.Format(Translate.Get("erroratline"), GameManager.dialoguepos, GameManager.scriptPath, string.Join("|", line), status, "syntax"));
+            return;
+        }
+    }
+    public string Working(string[] line)
+    {
+        return "";
+    }
+}
