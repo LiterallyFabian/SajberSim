ea67750fafcc5d36bfe1bd5b2bca663df2dda119
Fabian <lindgrenfabian@gmail.com>
2020-07-17 18:01:06 +0200

    add description to INovelAction interface

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -93,7 +93,16 @@ public class GameManager : MonoBehaviour
 
     public interface INovelAction
     {
+        /// <summary>
+        /// Runs the action if it passes the debug stage
+        /// </summary>
+        /// <param name="line">Full line to the action to run</param>
         void Run(string[] line);
+        /// <summary>
+        /// Debugs the action and returns appropriate feedback
+        /// </summary>
+        /// <param name="line">Full line to debug</param>
+        /// <returns>"" if everything works, else an error message</returns>
         string Working(string[] line);
     }
 
