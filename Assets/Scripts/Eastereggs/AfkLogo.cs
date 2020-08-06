75bd2e9b747e558a026e5f16781594a34dce1276
Fabian <lindgrenfabian@gmail.com>
2020-08-06 21:26:55 +0200

    added logo easter egg when idle

---
+++ Assets/Scripts/Eastereggs/AfkLogo.cs
@@ -0,0 +1,21 @@
+﻿using Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AfkLogo : MonoBehaviour
+{
+    public Image logoObj;
+    private Sprite logo;
+    public Sprite afklogo;
+    private void Start()
+    {
+        logo = logoObj.sprite;
+    }
+    private void Update()
+    {
+        if (SteamUtils.SecondsSinceComputerActive > 10) logoObj.sprite = afklogo;
+        else logoObj.sprite = logo;
+    }
+}
