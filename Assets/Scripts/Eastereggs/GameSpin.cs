2b0726ec222a53c87baa8f8ef3e0c89393a0bc09
Fabian <lindgrenfabian@gmail.com>
2020-08-06 21:55:38 +0200

    Updated fabina easter egg

--- Assets/Scripts/Eastereggs/GameSpin.cs
+++ Assets/Scripts/Eastereggs/GameSpin.cs
@@ -12,6 +12,7 @@ using UnityEngine.UI;
 public class GameSpin : MonoBehaviour
 {
     public AudioSource music;
+    public UnityEngine.UI.Image profilepic;
     public GameObject character;
     private bool running = false;
 
@@ -33,18 +34,24 @@ public class GameSpin : MonoBehaviour
         StartCoroutine(ChangeCharacter(1, 3, 13f));
         GameObject.Find("Canvas/logo").GetComponent<BeatPulse>().size = 1.5f;
         GameObject.Find("Canvas/logo").GetComponent<BeatPulse>().BPM /= 2;
-
+        GameObject.Find("Canvas/logo").GetComponent<Animator>().Play("logo away");
+        ParticleSystem.EmissionModule emission = GameObject.Find("ParticleHolder/Blossom").GetComponent<ParticleSystem>().emission;
+        ParticleSystem.ForceOverLifetimeModule force = GameObject.Find("ParticleHolder/Blossom").GetComponent<ParticleSystem>().forceOverLifetime;
+        emission.rateOverTime = 30;
+        force.x = -50;
         yield return new WaitForSeconds(1);
         GameObject.Find("Canvas/ButtonQuit").GetComponent<Button>().interactable = false;
-        GameObject.Find("Canvas/ButtonPlay").GetComponent<Text>().text = Translate.Get("help").ToLower();
+        string help = string.Join(" ", Translate.Get("help").ToLower().ToCharArray());
+        GameObject.Find("Canvas/ButtonPlay").GetComponent<Text>().text = help;
+        profilepic.transform.localScale = new Vector3(1, -1, 1);
         yield return new WaitForSeconds(1);
-        GameObject.Find("Canvas/ButtonLoad").GetComponent<Text>().text = Translate.Get("help").ToLower();
+        GameObject.Find("Canvas/ButtonLoad").GetComponent<Text>().text = help;
         string username = GameObject.Find("Canvas/Username").GetComponent<Text>().text;
         GameObject.Find("Canvas/Username").GetComponent<Text>().text = string.Format(Translate.Get("goodbyeuser"), Helper.UsernameCache());
         yield return new WaitForSeconds(1);
-        GameObject.Find("Canvas/ButtonFind").GetComponent<Text>().text = Translate.Get("help").ToLower();
+        GameObject.Find("Canvas/ButtonFind").GetComponent<Text>().text = help;
         yield return new WaitForSeconds(1);
-        GameObject.Find("Canvas/ButtonSettings").GetComponent<Text>().text = Translate.Get("help").ToLower();
+        GameObject.Find("Canvas/ButtonSettings").GetComponent<Text>().text = help;
         yield return new WaitForSeconds(1);
         GameObject.Find("Canvas/ButtonQuit").GetComponent<Text>().text = "h e l p m e";
         yield return new WaitForSeconds(1);
@@ -57,15 +64,16 @@ public class GameSpin : MonoBehaviour
         GameObject.Find("Canvas/ButtonFind").GetComponent<Text>().text = Translate.Get("novels");
         GameObject.Find("Canvas/ButtonLoad").GetComponent<Text>().text = Translate.Get("continue");
         GameObject.Find("Canvas/ButtonSettings").GetComponent<Text>().text = Translate.Get("settings");
-
         yield return new WaitForSeconds(1.4f);
-
+        profilepic.transform.localScale = Vector3.one;
         GameObject.Find("Canvas/Username").GetComponent<Text>().text = username;
         GameObject.Find("Canvas/ButtonQuit").GetComponent<Text>().text = Translate.Get("quit");
         GameObject.Find("Canvas/ButtonQuit").GetComponent<Button>().interactable = true;
         Camera.main.transform.rotation = Quaternion.identity;
         GameObject.Find("Canvas/logo").GetComponent<BeatPulse>().size = 1.055f;
         GameObject.Find("Canvas/logo").GetComponent<BeatPulse>().BPM *= 2;
+        emission.rateOverTime = 7;
+        force.x = 0;
         Destroy(this);
     }
     public IEnumerator ChangeAudio(float oldValue, float newValue, float duration)
